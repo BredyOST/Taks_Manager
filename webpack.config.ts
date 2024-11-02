b649e3df2cd8b9d65a76facfaf809d944ddbe77d
maksim <89292642644m@gmail.com>
2024-11-02 11:59:38 +0300

    добавлен основной конфг webpack

---
+++ webpack.config.ts
@@ -0,0 +1,25 @@
+import webpack from 'webpack';
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildMode, BuildPath, PORT} from "./config/build/types/config";
+import path from "path";
+
+export default (env:BuildEnv) => {
+
+    const paths: BuildPath = {
+        entry:'./src/index.tsx',
+        build: path.resolve(__dirname, 'dist'),
+        html: './src/index.html'
+    }
+    const buildMod: BuildMode = env.mode || 'development';
+    const PORT = +env.mode || 3000;
+    const isDev= buildMod === "development";
+
+    const config:webpack.Configuration = buildWebpackConfig({
+        mode: buildMod,
+        paths,
+        port: PORT,
+        isDev
+    })
+
+    return config
+};
